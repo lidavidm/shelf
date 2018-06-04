060203d150f421b7548088e7f62961c65b3b95f3
David Li <li.davidm96@gmail.com>
2018-06-04 10:08:38 -0700

    feat(web): fetch entries from server

--- shelf-server/static/src/App.vue
+++ shelf-server/static/src/App.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="app">
         <h1>{{ msg }}</h1>
+        <Items />
     </div>
 </template>
 
 <script>
+    import Items from "./Items.vue";
+
     export default {
         name: "app",
         data() {
             return {
                 msg: "Welcome to Your Vue.js App!",
             };
+        },
+        components: {
+            Items,
         },
     };
 </script>
---
+++ shelf-server/static/src/Items.vue
@@ -0,0 +1,41 @@
+<template>
+    <section id="items">
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="item in items">
+                    <td>{{ item["name"]["alternatives"][item["name"]["default"]] }}</td>
+                    <td>PTW</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "items",
+        data() {
+            return {
+                items: [],
+            };
+        },
+        mounted() {
+            window.fetch("/item")
+                  .then(r => r.json())
+                  .then((items) => {
+                      console.log(items);
+                      this.items = items;
+                  });
+        },
+    };
+</script>
+
+<style lang="css">
+</style>
