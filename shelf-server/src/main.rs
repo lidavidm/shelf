1dbff1b4c091b64400c7aa31414253fa36c41f15
David Li <li.davidm96@gmail.com>
2018-06-03 17:59:17 -0700

    feat(server): basic create/query items

--- shelf-server/src/main.rs
+++ shelf-server/src/main.rs
@@ -4,7 +4,7 @@ extern crate shelf;
 
 use std::sync::{Arc, RwLock};
 
-use actix_web::{App, HttpRequest, http, server};
+use actix_web::{App, HttpRequest, Json, http, server};
 
 struct AppState {
     shelf: Arc<RwLock<shelf::Shelf>>,
@@ -16,9 +16,26 @@ fn shutdown(req: HttpRequest<AppState>) -> String {
     "Goodbye!".to_owned()
 }
 
+fn begin(params: (Json<shelf::item::Item>, HttpRequest<AppState>)) -> String {
+    let (item, req) = params;
+    req.state().shelf.write().unwrap().insert_item(item.clone()).unwrap();
+    "created".to_owned()
+}
+
+fn items(req: HttpRequest<AppState>) -> Json<Vec<shelf::item::Item>> {
+    let shelf = req.state().shelf.read().unwrap();
+    let items = shelf.query_items().map(|x| x.1.clone()).collect();
+    Json(items)
+}
+
 fn main() {
+    let shelf_ref = Arc::new(RwLock::new(shelf::Shelf::new()));
     server::new(
-        || App::with_state(AppState { shelf: Arc::new(RwLock::new(shelf::Shelf::new())) })
+        move || App::with_state(AppState { shelf: shelf_ref.clone() })
             .resource("/shutdown", |r| r.method(http::Method::POST).f(shutdown))
+            .resource("/item", |r| {
+                r.method(http::Method::PUT).with(begin);
+                r.method(http::Method::GET).with(items);
+            })
     ).bind("127.0.0.1:8088").expect("Could not bind to port 8088").run();
 }
