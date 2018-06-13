15c45093efdf43631b38f2db97c1eee60509ffbb
David Li <li.davidm96@gmail.com>
2018-06-12 20:15:22 -0700

    feat(web): basic MAL import (single entry)

--- shelf-server/static/src/imports/malurl.js
+++ shelf-server/static/src/imports/malurl.js
@@ -1,5 +1,7 @@
 import moment from "moment";
 
+import * as util from "../util";
+
 export default function malurl(url) {
     return window.fetch(`/proxy?url=${url}`)
         .then(r => r.text())
@@ -8,7 +10,47 @@ export default function malurl(url) {
             const doc = parser.parseFromString(body, "text/html");
             const page = doc.documentElement;
 
-            const imported = [];
+            const title = page.querySelector("h1 *[itemprop=name]")
+                  .textContent
+                  .trim();
+
+            let kind = page.querySelectorAll(".js-scrollfix-bottom div > span + a")[0].textContent;
+            switch (kind) {
+            case "Movie":
+                kind = "Film";
+            }
+
+            const key = util.makeKey(kind, title);
+            const names = {
+                default: "English",
+                alternatives: {
+                    "English": title,
+                },
+            };
+
+            for (const node of page.querySelectorAll(".js-scrollfix-bottom .spaceit_pad")) {
+                const text = node.textContent.trim();
+                if (text.startsWith("Japanese:")) {
+                    names.alternatives["Japanese"] = text.slice(9).trim();
+                }
+            }
+
+            const imported = [{
+                key,
+                kind,
+                name: names,
+                people: [],
+                season: null,
+                entries: [],
+                status: "Planned",
+                rating: null,
+                added: moment().format(),
+                started: null,
+                completed: null,
+                extra: {
+                    mal_id: page.querySelector("#myinfo_anime_id").value,
+                },
+            }];
 
             return imported;
         });
