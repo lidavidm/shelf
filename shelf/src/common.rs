0cbbda0227c9dd442e7b9ec21501fb7270ddf80d
David Li <li.davidm96@gmail.com>
2020-02-17 16:24:26 -0500

    test(shelf): clean up DateBool tests

--- shelf/src/common.rs
+++ shelf/src/common.rs
@@ -165,48 +165,33 @@ mod tests {
     use chrono::{DateTime, NaiveDate};
     use serde_test::{assert_de_tokens, assert_tokens, Token};
 
-    #[test]
-    fn test_ser_datebool() {
-        assert_tokens(&DateBool::False, &[Token::None]);
-        assert_tokens(&DateBool::True, &[Token::Bool(true)]);
-        assert_tokens(
-            &DateBool::Timestamp(
-                DateTime::parse_from_rfc3339("1996-12-19T16:39:57-08:00").unwrap(),
-            ),
-            &[Token::String("1996-12-19T16:39:57-08:00")],
-        );
-        assert_tokens(
-            &DateBool::Date(NaiveDate::from_ymd(2018, 1, 1)),
-            &[Token::String("2018-01-01")],
-        );
-        assert_tokens(
-            &DateBool::YearMonth(2018, 1),
-            &[Token::String("2018-01-00")],
-        );
+    fn assert_ser_de<'de, T>(value: &T, tokens: &'de [Token])
+    where
+        T: serde::Serialize + serde::Deserialize<'de> + PartialEq + std::fmt::Debug,
+    {
+        assert_tokens(value, tokens);
+        assert_de_tokens(value, tokens);
     }
 
     #[test]
-    fn test_de_datebool() {
-        assert_de_tokens(&DateBool::False, &[Token::None]);
+    fn test_datebool_ser_de() {
+        assert_ser_de(&DateBool::False, &[Token::None]);
+        assert_ser_de(&DateBool::True, &[Token::Bool(true)]);
         assert_de_tokens(&DateBool::False, &[Token::String("0000-00-00")]);
         assert_de_tokens(&DateBool::False, &[Token::Bool(false)]);
         assert_de_tokens(&DateBool::False, &[Token::Unit]);
-        assert_de_tokens(&DateBool::True, &[Token::Bool(true)]);
-        assert_de_tokens(
+
+        assert_ser_de(
             &DateBool::Timestamp(
                 DateTime::parse_from_rfc3339("1996-12-19T16:39:57-08:00").unwrap(),
             ),
             &[Token::String("1996-12-19T16:39:57-08:00")],
         );
-        assert_de_tokens(
+        assert_ser_de(
             &DateBool::Date(NaiveDate::from_ymd(2018, 1, 1)),
             &[Token::String("2018-01-01")],
         );
-        assert_de_tokens(
-            &DateBool::Date(NaiveDate::from_ymd(2018, 1, 1)),
-            &[Token::String("2018/01/01")],
-        );
-        assert_de_tokens(
+        assert_ser_de(
             &DateBool::YearMonth(2018, 1),
             &[Token::String("2018-01-00")],
         );
