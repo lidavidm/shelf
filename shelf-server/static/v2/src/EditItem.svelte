524ed3ca0c99cf2e039e12e3dc670bcf0103d345
David Li <li.davidm96@gmail.com>
2020-10-29 16:22:17 -0400

    feat(web2): start implementing editing

--- shelf-server/static/v2/src/EditItem.svelte
+++ shelf-server/static/v2/src/EditItem.svelte
@@ -46,8 +46,6 @@
                 <input id="key" readonly type="text" value={item.key} />
             </div>
 
-            <!-- Name -->
-
             <div>
                 <label for="status">Status:</label>
                 <select id="status" bind:value={item.status}>
@@ -66,21 +64,37 @@
                 </select>
             </div>
 
-            <!-- Rating -->
+            <div>
+                <label for="rating">Rating</label>
+                <select id="rating" bind:value={item.rating}>
+                    <option :value="null">-</option>
+                    <option :value="0">0</option>
+                    <option :value="1">1</option>
+                    <option :value="2">2</option>
+                    <option :value="3">3</option>
+                    <option :value="4">4</option>
+                    <option :value="5">5</option>
+                    <option :value="6">6</option>
+                    <option :value="7">7</option>
+                    <option :value="8">8</option>
+                    <option :value="9">9</option>
+                    <option :value="10">10</option>
+                </select>
+            </div>
 
             <!-- Started -->
             <!-- Completed -->
 
+            <!-- Name -->
             <!-- People -->
             <!-- Series -->
             <!-- Tags -->
             <!-- Extra/URLs -->
 
-            <!-- Synopsis -->
-            <!-- Comments -->
-
-            <!-- Covers -->
+            <!-- Tabbed: -->
             <!-- Entries -->
+            <!-- Covers -->
+            <!-- Synopsis/Comments -->
         </section>
         <section class="buttons">
             <button>Cancel</button>
