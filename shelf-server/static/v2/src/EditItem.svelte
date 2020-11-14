8eddaa8cea0253b80dfac8c92e7013a9272cef27
David Li <li.davidm96@gmail.com>
2020-11-13 19:43:08 -0500

    fix(web2): properly edit rating

--- shelf-server/static/v2/src/EditItem.svelte
+++ shelf-server/static/v2/src/EditItem.svelte
@@ -81,18 +81,18 @@
             <div>
                 <label for="rating">Rating</label>
                 <select id="rating" bind:value={item.rating}>
-                    <option :value="null">-</option>
-                    <option :value="0">0</option>
-                    <option :value="1">1</option>
-                    <option :value="2">2</option>
-                    <option :value="3">3</option>
-                    <option :value="4">4</option>
-                    <option :value="5">5</option>
-                    <option :value="6">6</option>
-                    <option :value="7">7</option>
-                    <option :value="8">8</option>
-                    <option :value="9">9</option>
-                    <option :value="10">10</option>
+                    <option value={null}>-</option>
+                    <option value={0}>0</option>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
+                    <option value={7}>7</option>
+                    <option value={8}>8</option>
+                    <option value={9}>9</option>
+                    <option value={10}>10</option>
                 </select>
             </div>
 
