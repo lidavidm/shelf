771bb746440ee91af9a6789ecce4b9fffb762526
David Li <li.davidm96@gmail.com>
2021-04-13 21:21:58 -0400

    feat(web2): expand dynasty-scans import

--- shelf-server/static/v2/src/import/dynastyscans.mjs
+++ shelf-server/static/v2/src/import/dynastyscans.mjs
@@ -22,54 +22,112 @@ export default async function dynastyScans(
     const body = await proxy(rawUrl);
     const document = util.parse(body);
 
-    // TODO: also implement a path to import a series
-
-    const title = document.querySelector("#chapter-title b").textContent.trim();
-    const author = document
-        .querySelector("#chapter-title > a")
-        .textContent.trim();
-
-    const key = `manga-${util.titleToKey(author)}-${util.titleToKey(title)}`;
-
-    const names = {
-        default: "English",
-        alternatives: {
-            English: title,
-        },
-    };
-
-    const entries = [];
-    template.key = key;
-    template.kind = "Manga";
-    template.name = names;
-    template.added = formatISO(new Date());
-    template.started = template.added;
-    template.completed = template.added;
-    template.publication_status = "Complete";
     const url = new URL(rawUrl);
     url.hash = "";
+
+    template.kind = "Manga";
+    template.added = formatISO(new Date());
+    template.started = template.added;
     template.extra = {
         external_url: url.toString(),
     };
-    template.status = "Completed";
-    template.tags = ["Oneshot", "Yuri"];
-    template.entries = [
-        {
-            number: 1,
-            volume: null,
-            name: {
-                default: "English",
-                alternatives: {
-                    English: "Oneshot",
-                },
+    template.tags = ["Yuri"];
+    if (url.pathname.startsWith("/series/")) {
+        const title = document
+            .querySelector("h2.tag-title b")
+            .textContent.trim();
+        const author = document
+            .querySelector("h2.tag-title > a")
+            .textContent.trim();
+        const key = `manga-${util.titleToKey(author)}-${util.titleToKey(
+            title
+        )}`;
+        const names = {
+            default: "English",
+            alternatives: {
+                English: title,
             },
-            completed: template.added,
-            extra: null,
-        },
-    ];
-    url.pathname = document.querySelector("#image img").getAttribute("src");
-    return {
-        cover: url.toString(),
-        item: template,
-    };
+        };
+        const entries = [];
+        let volume = null;
+        let number = 1;
+        for (const entryEl of document.querySelectorAll(".chapter-list > *")) {
+            if (entryEl.tagName === "DT") {
+                volume = parseInt(
+                    entryEl.textContent.trim().match(/Volume (\d)+/)[1],
+                    10
+                );
+            } else if (entryEl.tagName === "DD") {
+                entries.push({
+                    number,
+                    volume,
+                    name: {
+                        default: "English",
+                        alternatives: {
+                            English: entryEl
+                                .querySelector("a")
+                                .textContent.trim(),
+                        },
+                    },
+                    completed: null,
+                    extra: null,
+                });
+                number++;
+            }
+        }
+        template.key = key;
+        template.name = names;
+        template.completed = null;
+        template.publication_status = "Publishing";
+        template.status = "InProgress";
+        template.entries = entries;
+        url.pathname = document.querySelector(".cover img").getAttribute("src");
+        return {
+            cover: url.toString().replace(/%3F/g, "?"),
+            item: template,
+        };
+    } else if (url.pathname.startsWith("/chapters/")) {
+        const title = document
+            .querySelector("#chapter-title b")
+            .textContent.trim();
+        const author = document
+            .querySelector("#chapter-title > a")
+            .textContent.trim();
+        const key = `manga-${util.titleToKey(author)}-${util.titleToKey(
+            title
+        )}`;
+        const names = {
+            default: "English",
+            alternatives: {
+                English: title,
+            },
+        };
+        template.key = key;
+        template.name = names;
+        template.completed = template.added;
+        template.publication_status = "Complete";
+        template.status = "Completed";
+        template.tags.push("Oneshot");
+        template.tags.sort();
+        template.entries = [
+            {
+                number: 1,
+                volume: null,
+                name: {
+                    default: "English",
+                    alternatives: {
+                        English: "Oneshot",
+                    },
+                },
+                completed: template.added,
+                extra: null,
+            },
+        ];
+        url.pathname = document.querySelector("#image img").getAttribute("src");
+        return {
+            cover: url.toString(),
+            item: template,
+        };
+    }
+    throw new Error(`Cannot import ${rawUrl}`);
 }
--- shelf-server/static/v2/src/import/dynastyscans.test.mjs
+++ shelf-server/static/v2/src/import/dynastyscans.test.mjs
@@ -3,9 +3,10 @@ import test from "ava";
 import importTitle from "./dynastyscans.mjs";
 import * as testUtil from "./test-util.mjs";
 
-const proxy = testUtil.makeProxy("test.beginning-their-new-life-together.html");
-
 test("import Dynasty Scans oneshot", async (t) => {
+    const proxy = testUtil.makeProxy(
+        "test.beginning-their-new-life-together.html"
+    );
     const url =
         "https://dynasty-scans.com/chapters/beginning_their_new_life_together#1";
     const { cover, item } = await importTitle(url, {
@@ -37,5 +38,74 @@ test("import Dynasty Scans oneshot", async (t) => {
     t.deepEqual(
         cover,
         "https://dynasty-scans.com/system/releases/000/031/588/00001.jpeg"
+    );
+});
+
+test("import Dynasty Scans series", async (t) => {
+    const proxy = testUtil.makeProxy("test.march-comes-in-like-a-lion.html");
+    const url = "https://dynasty-scans.com/series/march_comes_in_like_a_lion";
+    const { cover, item } = await importTitle(url, {
+        template: { entries: [] },
+        proxy,
+    });
+    t.deepEqual(item.key, "manga-umino-chika-march-comes-in-like-a-lion");
+    t.deepEqual(item.kind, "Manga");
+    t.deepEqual(item.name.default, "English");
+    t.deepEqual(item.added, item.started);
+    t.deepEqual(null, item.completed);
+    t.deepEqual(
+        item.name.alternatives[item.name.default],
+        "March Comes in Like a Lion"
+    );
+    t.deepEqual(item.entries.length, 33);
+    t.deepEqual(item.entries[0].number, 1);
+    t.deepEqual(item.entries[0].volume, 1);
+    t.deepEqual(item.entries[0].name.default, "English");
+    t.deepEqual(
+        item.entries[0].name.alternatives.English,
+        "Chapter 1: Kiriyama Rei"
+    );
+    t.deepEqual(item.entries[0].completed, null);
+
+    t.deepEqual(item.entries[22].number, 23);
+    t.deepEqual(item.entries[22].volume, 2);
+    t.deepEqual(item.entries[22].name.default, "English");
+    t.deepEqual(item.entries[22].name.alternatives.English, "Vol 2 Omake");
+    t.deepEqual(item.entries[22].completed, null);
+
+    t.deepEqual(item.extra.external_url, url);
+    t.deepEqual(
+        cover,
+        "https://dynasty-scans.com/system/tag_contents_covers/000/001/434/medium/3gatsu01.jpg?1359562488"
+    );
+});
+
+test("import Dynasty Scans series without volumes", async (t) => {
+    const proxy = testUtil.makeProxy("test.can-a-guy-like-me-be-a-maid.html");
+    const url = "https://dynasty-scans.com/series/can_a_guy_like_me_be_a_maid";
+    const { cover, item } = await importTitle(url, {
+        template: { entries: [] },
+        proxy,
+    });
+    t.deepEqual(item.key, "manga-koshou-can-a-guy-like-me-be-a-maid");
+    t.deepEqual(item.kind, "Manga");
+    t.deepEqual(item.name.default, "English");
+    t.deepEqual(item.added, item.started);
+    t.deepEqual(null, item.completed);
+    t.deepEqual(
+        item.name.alternatives[item.name.default],
+        "Can a Guy Like Me Be a Maid?"
+    );
+    t.deepEqual(item.entries.length, 15);
+    t.deepEqual(item.entries[0].number, 1);
+    t.deepEqual(item.entries[0].volume, null);
+    t.deepEqual(item.entries[0].name.default, "English");
+    t.deepEqual(item.entries[0].name.alternatives.English, "Chapter 0");
+    t.deepEqual(item.entries[0].completed, null);
+
+    t.deepEqual(item.extra.external_url, url);
+    t.deepEqual(
+        cover,
+        "https://dynasty-scans.com/system/tag_contents_covers/000/015/043/medium/[Hachimitsu_Scans]_Bokunare_c01_00.jpg?1604022009"
     );
 });
